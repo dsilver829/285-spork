09e013b9783fd33dd04837c39b48f9ba3f3ef5dc
David Silver <dsilver@candidatemetrics.com>
2012-09-04 12:56:44 -0700

    allow focus filter in RSpec

--- auth/spec/spec_helper.rb
+++ auth/spec/spec_helper.rb
@@ -22,6 +22,10 @@ Spork.prefork do
     config.use_transactional_fixtures = true
     config.include(MailerMacros)
     config.before(:each) { reset_email }
+
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.filter_run focus: true
+    config.run_all_when_everything_filtered = true
   end
 
 end
